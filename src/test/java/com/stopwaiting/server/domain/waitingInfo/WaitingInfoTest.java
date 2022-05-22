816dc6478a02514fb630841099109b463282b840
giyun <dkwhkp0@naver.com>
2022-05-23 01:54:29 +0900

    ec2

--- src/test/java/com/stopwaiting/server/domain/waitingInfo/WaitingInfoTest.java
+++ src/test/java/com/stopwaiting/server/domain/waitingInfo/WaitingInfoTest.java
@@ -12,7 +12,7 @@ import java.util.Set;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
-@SpringBootTest
+//@SpringBootTest
 class WaitingInfoTest {
 
     @Autowired
--- src/test/java/com/stopwaiting/server/web/UserApiControllerTest.java
+++ src/test/java/com/stopwaiting/server/web/UserApiControllerTest.java
@@ -12,6 +12,7 @@ import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
